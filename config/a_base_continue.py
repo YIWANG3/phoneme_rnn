ec4b5b075de877d3574e4950c443e6263f097334
yiwang <yiwang3@andrew.cmu.edu>
2020-04-01 21:25:46 -0700

    update

--- config/a_base_continue.py
+++ config/a_base_continue.py
@@ -6,7 +6,7 @@ config = edict()
 config.name = "a_base"
 config.batch_size = 64
 config.epoch = 100
-config.lr = 2e-4
+config.lr = 0.001
 config.wd = 5e-5
 config.hidden_size = 512
 config.model_name = "a_base"
@@ -18,5 +18,7 @@ config.save_freq = 1
 config.MODEL_DIR = 'saved_models'
 config.MODEL_FOLDER_NAME = 'a_base'
 config.MODEL_SAVE_PATH = os.path.join(config.MODEL_DIR, config.MODEL_FOLDER_NAME)
-config.optim = "adam"
+config.optim = "sgd"
 config.schedule = False
+config.continue_model_path = os.path.join(config.MODEL_DIR, config.MODEL_FOLDER_NAME,
+                                          "a_base__EP-15__04-02_02-47-11__10.510849909584087__.model")
