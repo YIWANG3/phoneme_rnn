340f979a779f011afc0be41c5b542ddbaa35c095
yiwang <yiwang3@andrew.cmu.edu>
2020-04-04 12:07:05 -0700

    update

---
+++ config/a_enhanced.py
@@ -0,0 +1,21 @@
+import os
+from easydict import EasyDict as edict
+
+config = edict()
+
+config.name = "a_enhanced"
+config.batch_size = 64
+config.epoch = 100
+config.lr = 2e-4
+config.wd = 5e-5
+config.hidden_size = 1024
+config.model_name = "a_enhanced"
+config.band_width = 40
+config.num_classes = 47
+config.print_freq = 10
+config.val_freq = 5
+config.save_freq = 1
+config.MODEL_DIR = 'saved_models'
+config.MODEL_FOLDER_NAME = 'a_enhanced'
+config.MODEL_SAVE_PATH = os.path.join(config.MODEL_DIR, config.MODEL_FOLDER_NAME)
+config.optim = "adam"
